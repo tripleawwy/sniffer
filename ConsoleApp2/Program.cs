a414cbb1eb799760d1d8a7af26168f3dadd44f9a
bjoerN <bjoern.marten@hotmail.de>
2018-10-12 14:40:42 +0200

    mein sniffer

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -94,7 +94,7 @@ namespace ConsoleApp2
 
             int arsch = 42;
 
-            Console.WriteLine("Arsssch = " + arsch);
+            Console.WriteLine("Arssschs = " + arsch);
             Console.WriteLine("Arsch = " + arsch++);
             Console.WriteLine("Arsch = " + ++arsch);
             Console.Read();
